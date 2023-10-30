9827d87eecc46d3800055a1213700f1fcdb74eeb
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-10-30 21:51:59 +0700

    p1

--- Social-Network-API/src/main/java/com/example/Social/Network/API/SocialNetworkApiApplication.java
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/SocialNetworkApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialNetworkApiApplication {
+public class  SocialNetworkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApiApplication.class, args);
