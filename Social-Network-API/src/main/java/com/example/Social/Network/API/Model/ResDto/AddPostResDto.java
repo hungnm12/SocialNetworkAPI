71695ce787b2501e69a216df7d1c2a06d53cc403
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-11-16 11:54:42 +0700

    cmt 2x p

--- Social-Network-API/src/main/java/com/example/Social/Network/API/Model/ResDto/AddPostResDto.java
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/Model/ResDto/AddPostResDto.java
@@ -1,13 +1,22 @@
 package com.example.Social.Network.API.Model.ResDto;
 
+import com.example.Social.Network.API.Model.Entity.Post;
 import com.example.Social.Network.API.Model.Entity.User;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddPostResDto {
-    @Id
-    private User Id;
+
+    private Long Id;
 
     private String url;
 
-    private User coins;
+    private Integer coins;
+
+
 }
--- Social-Network-API/src/main/java/com/example/Social/Network/API/Service/Impl/PostServiceImpl.java
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/Service/Impl/PostServiceImpl.java
@@ -1,20 +1,26 @@
 package com.example.Social.Network.API.Service.Impl;
 
+import com.example.Social.Network.API.Constant.ResponseCode;
+import com.example.Social.Network.API.Constant.ResponseMessage;
 import com.example.Social.Network.API.Exception.ResponseException;
 import com.example.Social.Network.API.Model.Entity.Post;
+import com.example.Social.Network.API.Model.Entity.User;
+import com.example.Social.Network.API.Model.ResDto.AddPostResDto;
 import com.example.Social.Network.API.Model.ResDto.GeneralResponse;
 import com.example.Social.Network.API.Repository.PostRepo;
 import com.example.Social.Network.API.Repository.UserRepo;
 import com.example.Social.Network.API.Service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+
+import static com.example.Social.Network.API.utils.JwtUtils.extractUserDetailsFromToken;
 
 @Service
 @Slf4j
@@ -38,9 +44,25 @@ private UserRepo userRepo;
         post1.setVideo(video);
         post1.setUrl(generatePostUrl());
 
+        User user = getUserFromToken(token);
+        post1.setUser(user);
+        chargeOnPost(user, post1);
         postRepo.save(post1);
 
-        return new GeneralResponse();
+        return new GeneralResponse(ResponseCode.OK_CODE, ResponseMessage.OK_CODE, (post1.getId() + post1.getUrl() + user.getCoins()));
+    }
+
+    private User getUserFromToken(String token) {
+        // Extract the user information from the token
+        UserDetails userDetails = extractUserDetailsFromToken(token);
+
+        // Get the user's email
+        String email = userDetails.getUsername();
+
+        // Find the user by email
+        User user = userRepo.findByEmail(email).get();
+
+        return user;
     }
 
     private String generatePostUrl() {
@@ -51,10 +73,64 @@ private UserRepo userRepo;
         String uuidString = uuid.toString();
 
         // Create a URL using the UUID string
-        String url = "https://example.com/posts/" + uuidString;
+        String url =  uuidString;
 
         return url;
     }
+
+
+    private void chargeOnPost(User user, Post post) {
+
+        int currentCoin = user.getCoins();
+
+        currentCoin -= 1;
+
+        user.setCoins(currentCoin);
+
+        userRepo.save(user);
+
+    }
+
+    @Override
+    public GeneralResponse getPost(String token, Long Id) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return new GeneralResponse();
+    }
+
+    @Override
+    public GeneralResponse editPost(String token, Long Id, String described, String status, File image, String image_del, String image_sort, File video, String auto_accept) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse deletePost(String token, Long Id) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse reportPost(String token, Long Id, String subject, String details) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse feel(String token, Long Id, String type) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse editPost(String token, Long Id, String described) {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse getMarkComment(Long Id, String index, String count) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
+    @Override
+    public GeneralResponse setMarkComment(String token, Long id, String content, String index, String count, String markId, String type) throws ResponseException, ExecutionException, InterruptedException, TimeoutException {
+        return null;
+    }
+
 
 
 
