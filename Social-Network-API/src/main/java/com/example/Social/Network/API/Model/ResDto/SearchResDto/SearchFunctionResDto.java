81d1394c8607ded84e7d6f19c019d93777de6fa7
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-11-20 00:58:21 +0700

    push code post aaaaa

---
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/Model/ResDto/SearchResDto/SearchFunctionResDto.java
@@ -0,0 +1,11 @@
+package com.example.Social.Network.API.Model.ResDto.SearchResDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchFunctionResDto {
+}
--- Social-Network-API/src/main/java/com/example/Social/Network/API/Repository/PostRepo.java
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/Repository/PostRepo.java
@@ -1,7 +1,12 @@
 package com.example.Social.Network.API.Repository;
 
 import com.example.Social.Network.API.Model.Entity.Post;
+import com.example.Social.Network.API.Model.ResDto.SearchResDto.SearchFunctionResDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +16,21 @@ import java.util.Optional;
 public interface PostRepo extends JpaRepository<Post, Long>, PagingAndSortingRepository<Post,Long> {
 
 Post findAllById(long Id);
+
+@Query(
+
+//        "SELECT p.user_id, p.post_id, p.described " +
+//                "from User u"
+//        + "Join Post p On u.user_id = p.user_id" +
+//                "where "
+
+
+
+)
+
+    Page<SearchFunctionResDto> search(@Param("token") String token,
+                                      @Param("keyword") String keyword,
+                                      @Param("user_id") String Id,
+                                      Pageable pageable
+                                      );
 }
