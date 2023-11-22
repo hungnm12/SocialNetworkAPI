b160ebf9919b1e21829434995c30d3bf38dd1670
lethanhdathust <dat.lt207661@sis.hust.edu.vn>
2023-11-23 00:05:37 +0700

    m

--- Social-Network-API/src/main/java/com/example/Social/Network/API/Service/Impl/PostServiceImpl.java
+++ Social-Network-API/src/main/java/com/example/Social/Network/API/Service/Impl/PostServiceImpl.java
@@ -173,7 +173,6 @@ private UserRepo userRepo;
         getPostResDto.setTrust(String.valueOf(Long.valueOf(post.getTrust())));
         getPostResDto.setIsMarked(String.valueOf(post.isMarked()));
         getPostResDto.setIsRated(String.valueOf(post.isRated()));
-        getPostResDto.setAuthor(author);
 //        getPostResDto.setImage((post.getImages().toString()));
 
 
